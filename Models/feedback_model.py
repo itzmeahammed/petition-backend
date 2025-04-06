a2474977ea37f833b3c7f06613ddcaf17a75b0f2
ahammed-wizzgeeks <ahammedmass24@gmail.com>
2025-04-06 13:18:56 +0530

    ad

--- Models/feedback_model.py
+++ Models/feedback_model.py
@@ -17,8 +17,8 @@ class Feedback(Document):
     def to_json(self):
         return {
             "id": str(self.id),
-            "user": self.user,
-            "petition":self.petition if self.petition else None,
+            "user": str(self.user.id),
+            "petition":str(self.petition.id) if self.petition else None,
             "feedback":self.feedback if self.feedback else None,
             "created_at":self.created_at if self.created_at else None,
 
